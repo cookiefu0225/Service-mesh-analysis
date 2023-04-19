2e276ccd6068990a5b29d8127ad31b3121b4ffec
cookiefu0225 <cfyang2@illinois.edu>
2023-04-18 23:05:54 -0500

    Frontend: Axios set up

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import RequestButton from './RequestButton.tsx';
+import RequestButton from './RequestButton';
 
 function App() {
   return (
--- frontend/src/RequestButton.tsx
+++ frontend/src/RequestButton.tsx
@@ -1,13 +1,28 @@
 import React, { useState } from 'react';
 import { Button, Space } from 'antd';
 import type { SizeType } from 'antd/es/config-provider/SizeContext';
+import axios from 'axios';
 
 const RequestButton: React.FC = () => {
   const [size, setSize] = useState<SizeType>('large'); // default is 'middle'
+	const url = 'https://api.coingecko.com/api/v3/ping'; // destination of request
 
+	// send request when button is clicked
 	const send = () => {
 		// Send request to backend
-		console.log("Clicked")
+		console.log("Clicked, see the request result");
+
+		// Test response
+		getRequest();
+		getRequest();
+		getRequest();
+		getRequest();
+	};
+
+	const getRequest = () => {
+		axios.get(url).then(response => {
+			console.log(response.data);
+		});
 	}
 
   return (
