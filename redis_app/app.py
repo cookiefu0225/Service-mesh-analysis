a39dd063b1ab4ae0b359bb0f8a483c296eea3e24
cookiefu0225 <cfyang2@illinois.edu>
2023-05-07 16:04:12 -0500

    Cilium

--- redis_app/app.py
+++ redis_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_file, jsonify
+from flask import Flask, send_file, jsonify, request
 import redis
 import io
 from pathlib import Path
@@ -13,11 +13,18 @@ with open(file_path, 'rb') as f:
     content = f.read()
     r.set('my_file', content)
 
-@app.route("/")
+@app.route("/", methods = ['GET', 'POST'])
 def hello_world():
-    return jsonify(
-        message='hello CS538\n'
-    )
+    if request.method == 'GET':
+        return jsonify(
+            message='hello CS538\n'
+        )
+    
+    if request.method == 'POST':
+        data = request.form['message']
+        return jsonify(
+            message=f'POST request received, your message: {data}'
+        )
 
 @app.route("/download")
 def file_request():
